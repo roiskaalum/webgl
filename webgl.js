a7886b6cb64390a84e4c4437274d2391b470b0c1
Rói Skaalum <roi.skaalum@gmail.com>
2025-04-24 13:21:26 +0200

    UV added

--- webgl.js
+++ webgl.js
@@ -27,6 +27,10 @@ document.getElementById('gl').addEventListener('mousemove', function(e)
     mouseY = e.y;
 });
 
+var textureGL = 0;
+var display = [0.0, 0.0, 0.0, 0.0];
+var displayGL = 0;
+
 // #region Init
 
 function InitWebGL()
@@ -145,7 +149,12 @@ function CreateGeometryBuffers(program)
 
     angleGL = gl.getUniformLocation(program, 'Angle');
 
+    CreateTexture(program, 'img/texture.png');
+
     gl.useProgram(program);
+    gl.uniform4fv(angleGL, new Float32Array(angle)); // Perhaps this will lead to errors down the line.
+
+    gl.uniform4fv(displayGL, new Float32Array(display));
 
     Render();
 }
@@ -216,7 +225,7 @@ function CreateVBO(program, vert)
     let vbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bufferData(gl.ARRAY_BUFFER,vert,gl.STATIC_DRAW);
-    const s = 6 * Float32Array.BYTES_PER_ELEMENT;
+    const s = 8 * Float32Array.BYTES_PER_ELEMENT;
 
     let p = gl.getAttribLocation(program, 'Pos');
     gl.vertexAttribPointer(p,3,gl.FLOAT,gl.FALSE,s,0);
@@ -226,6 +235,11 @@ function CreateVBO(program, vert)
     let c = gl.getAttribLocation(program, 'Color');
     gl.vertexAttribPointer(c,3,gl.FLOAT,gl.FALSE,s,o);
     gl.enableVertexAttribArray(c);
+
+    const o2 = o * 2;
+    let u = gl.getAttribLocation(program, 'UV');
+    gl.vertexAttribPointer(u,2,gl.FLOAT,gl.FALSE,s,o2);
+    gl.enableVertexAttribArray(u);
 }
 
 function Render()
@@ -233,45 +247,47 @@ function Render()
     gl.clearColor(0.0, 0.4, 0.6, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT |
              gl.DEPTH_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 6);
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 8);
 }
 
 
 // #region Add...
 
-function AddVertex(x, y, z, r, g, b)
+function AddVertex(x, y, z, r, g, b, u, v)
 {
     const index = vertices.length;
-    vertices.length += 6;
+    vertices.length += 8;
     vertices[index + 0] = x;
     vertices[index + 1] = y;
     vertices[index + 2] = z;
     vertices[index + 3] = r;
     vertices[index + 4] = g;
     vertices[index + 5] = b;
+    vertices[index + 6] = u;
+    vertices[index + 7] = v;
 }
 
-function AddTriangle(x1, y1, z1, r1, g1, b1,
-                     x2, y2, z2, r2, g2, b2,
-                     x3, y3, z3, r3, g3, b3)
+function AddTriangle(x1, y1, z1, r1, g1, b1, u1, v1,
+                     x2, y2, z2, r2, g2, b2, u2, v2,
+                     x3, y3, z3, r3, g3, b3, u3, v3)
 {
-    AddVertex(x1, y1, z1, r1, g1, b1);
-    AddVertex(x2, y2, z2, r2, g2, b2);
-    AddVertex(x3, y3, z3, r3, g3, b3);
+    AddVertex(x1, y1, z1, r1, g1, b1, u1, v1);
+    AddVertex(x2, y2, z2, r2, g2, b2, u2, v2);
+    AddVertex(x3, y3, z3, r3, g3, b3, u3, v3);
 }
 
-function AddQuad(x1, y1, z1, r1, g1, b1,
-                 x2, y2, z2, r2, g2, b2,
-                 x3, y3, z3, r3, g3, b3,
-                 x4, y4, z4, r4, g4, b4)
+function AddQuad(x1, y1, z1, r1, g1, b1, u1, v1,
+                 x2, y2, z2, r2, g2, b2, u2, v2,
+                 x3, y3, z3, r3, g3, b3, u3, v3,
+                 x4, y4, z4, r4, g4, b4, u4, v4)
 {
-    AddTriangle(x1,y1,z1,r1,g1,b1,
-                x2,y2,z2,r2,g2,b2,
-                x3,y3,z3,r3,g3,b3);
+    AddTriangle(x1, y1, z1, r1, g1, b1, u1, v1,
+                x2, y2, z2, r2, g2, b2, u2, v2,
+                x3, y3, z3, r3, g3, b3, u3, v3);
 
-    AddTriangle(x3,y3,z3,r3,g3,b3,
-                x4,y4,z4,r4,g4,b4,
-                x1,y1,z1,r1,g1,b1);
+    AddTriangle(x3, y3, z3, r3, g3, b3, u3, v3,
+                x4, y4, z4, r4, g4, b4, u4, v4,
+                x1, y1, z1, r1, g1, b1, u1, v1);
 }
 
 function AddSubdividedQuad(v1, v2, v3, v4, baseColor, divisions = 1) {
@@ -400,9 +416,9 @@ function CreateTriangle(width, height, colors)
     vertices.length = 0;
     const w = width * 0.5;
     const h = height * 0.5;
-    AddTriangle(0.0,  h, 0.0, ...colors[0],
-                 -w, -h, 0.0, ...colors[1],
-                  w, -h, 0.0, ...colors[2],);
+    AddTriangle(0.0,  h, 0.0, ...colors[0], 0.5, 1.0,
+                 -w, -h, 0.0, ...colors[1], 0.0, 0.0,
+                  w, -h, 0.0, ...colors[2], 1.0, 0.0);
 }
 
 function CreateQuad(width, height, colors)
@@ -410,10 +426,10 @@ function CreateQuad(width, height, colors)
     vertices.length = 0;
     const w = width * 0.5;
     const h = height * 0.5;
-    AddQuad(-w, h, 0.0, ...colors[0],
-            -w,-h, 0.0, ...colors[1],
-             w,-h, 0.0, ...colors[2],
-             w, h, 0.0, ...colors[3]);
+    AddQuad(-w, h, 0.0, ...colors[0], 0.0, 1.0,
+            -w,-h, 0.0, ...colors[1], 0.0, 0.0,
+             w,-h, 0.0, ...colors[2], 1.0, 0.0,
+             w, h, 0.0, ...colors[3], 1.0, 1.0);
 }
 
 function CreateCube(width, height, depth, colors)
@@ -635,4 +651,60 @@ function CreateColorElements(numberOfColorElements)
         div.appendChild(input);
         colorsContainer.appendChild(div);
     }
+}
+
+function CreateTexture(prog, url)
+{
+    const texture = LoadTexture(url);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    textureGl = gl.getUniformLocation(prog, 'Texture');
+    displayGL = gl.getUniformLocation(prog, 'Display');
+}
+
+function LoadTexture(url)
+{
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    const pixel = new Uint8Array([0, 0, 255, 255]);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0,
+    gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+    const image = new Image();
+    image.onload = () => {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
+    gl.RGBA, gl.UNSIGNED_BYTE, image);
+    SetTextureFilters(image);
+    };
+    image.src = url;
+    return texture;
+}
+
+function SetTextureFilters(image)
+{
+    if (IsPow2(image.width) && IsPow2(image.height))
+    {
+        gl.generateMipmap(gl.TEXTURE_2D);
+    }
+    else
+    {
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+}
+
+function IsPow2(value)
+{
+    return (value & (value - 1)) === 0;
+}
+
+function Update()
+{
+    const t = document.getElementById('texture');
+    display[3] = t.checked ? 1.0 : 0.0;
+
+    gl.uniform4fv(displayGL, new Float32Array(display));
+    Render();
 }
